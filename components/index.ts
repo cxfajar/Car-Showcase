94da4d04bbf539164658ea3bac6e1c7669cd6208
Fajar Pervaiz <fajarpervaiz197@gmail.com>
2023-11-16 16:20:13 +0500

    correct errors

--- components/index.ts
+++ components/index.ts
@@ -1,11 +1,11 @@
 import CarCard from "./CarCard";
 import CustomButton from "./CustomButton";
 import CustomFilter from "./CustomFilter";
-import Footer from "./Footer";
+import Footer from "./footer";
 import NavBar from "./Navbar";
 import ShowMore from "./ShowMore";
-import SearchBar from "./Searchbar";
-import Hero from "./Hero";
+import SearchBar from "./SearchBar";
+import Hero from "./hero";
 
 export {
   Hero,
